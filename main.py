26295967ca2b00eaea9f4689e2eb5a907f3f26a4
даниэль <dannilboyar@gmail.com>
2024-09-29 21:14:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+num1 = int(input("Введите первое число: "))
+num2 = int(input("Введите второе число: "))
+if num1 == num2:
+  print("Числа равны.")
+else:
+  print(f"Наименьшее число: {min(num1, num2)}")
+
+
